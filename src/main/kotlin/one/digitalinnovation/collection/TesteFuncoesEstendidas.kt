0b5f58a9f2fdd8ca5f8741a5c7f2d6ebf1ee8773
Victoria Priscila <victoria.priscila.pl@gmail.com>
2021-08-19 16:12:30 -0300

    Last part

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteFuncoesEstendidas.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal()
+    )
+    println("--------------------------")
+    println(salarios.somatoria())
+    println(salarios.media())
+}
